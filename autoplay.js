73e6b8b074e21a464c74707c1b6419bb654eac38
Jackie Nguyen <jnguye89@gmail.com>
2023-05-21 23:04:42 -0500

    Update event listener to click

--- autoplay.js
+++ autoplay.js
@@ -1,9 +1,9 @@
 (function () {
   var audio = document.createElement("audio");
-  document.body.appendChild(audio);
-  audio.src = "./RustyNail.mp3";
+document.body.appendChild(audio);
+audio.src = "./RustyNail.mp3";
 
-  document.body.addEventListener("mousemove", function () {
+document.body.addEventListener("click", function () {
     audio.play();
-  });
+});
 })();
